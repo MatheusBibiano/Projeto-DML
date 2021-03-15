84a3a7b62b1c7fa32ff7bf30cf75f027590d9fe7
Matheus Bibiano Alves <66566761+MatheusBibiano@users.noreply.github.com>
2021-03-15 17:26:52 -0300

    minor bug fixes

--- src/script/menuButton.js
+++ src/script/menuButton.js
@@ -1,15 +1,29 @@
 const button = document.querySelector(".navbar-toggler");
 const iconDefault = document.querySelector("#default");
 const iconClose = document.querySelector("#close");
+const navOptions = document.querySelectorAll(".nav-options");
 
-iconClose.style.display = "none";
+function setState() {
+    iconClose.style.display = "none";
 
-button.addEventListener("click", () => {
-    if (iconClose.style.display == "none") {
-        iconClose.style.display = "flex";
-        iconDefault.style.display = "none";
-    } else {
-        iconDefault.style.display = "flex";
-        iconClose.style.display = "none";
+    button.addEventListener("click", () => {
+        if (iconClose.style.display == "none") {
+            iconClose.style.display = "flex";
+            iconDefault.style.display = "none";
+        } else {
+            iconDefault.style.display = "flex";
+            iconClose.style.display = "none";
+        }
+    });
+}
+
+function closeOnClick() {
+    for (let i = 0; i < navOptions.length; i++) {
+        navOptions[i].addEventListener("click", () => {
+            button.click();
+        });
     }
-});
+}
+
+setState();
+closeOnClick();
